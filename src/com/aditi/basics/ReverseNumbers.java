acf1636a4e1062468b6af2bc35d104a2c5d80262
Aditi <aditi2805sharma@gmail.com>
2026-02-10 10:30:56 +0530

    commit file changes and creation

---
+++ src/com/aditi/basics/ReverseNumbers.java
@@ -0,0 +1,19 @@
+package com.aditi.basics;
+import java.util.Scanner;
+
+public class ReverseNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        System.out.println("Reversed number: " + reverse);
+    }
+}
